53091aa1e247ebc46aa8296ba0b6e9d27ef4d72a
sunnydodti <sunnydodti@gmail.com>
2025-01-15 17:02:37 +0530

    sing apk script

---
+++ scripts/android/sign-akp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# $1 = apk dir
+# $2 = suffix - "-dev"
+# $3 = version
+set -e
+
+if [ -z "$1" ]; then
+  echo "reading in base dir ." 
+  $1 = "."
+fi
+
+APK_DIR="$1/build/app/outputs/flutter-apk"
+
+apksigner sign  \
+  -v \
+  --in $APK_DIR/app-armeabi-v7a$2-release.apk \
+  --ks release-keystore.jks \
+  --ks-key-alias ${{ vars.KEY_ALIAS }}\
+  --ks-pass env:KEYSTORE_PASSWORD \
+  --key-pass env:KEY_PASSWORD
