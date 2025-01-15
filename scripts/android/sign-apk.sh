469cbaa8a76de0b51e1bc248d219403d5d2168ca
sunnydodti <sunnydodti@gmail.com>
2025-01-15 17:14:19 +0530

    change path to apksigner

--- scripts/android/sign-apk.sh
+++ scripts/android/sign-apk.sh
@@ -11,7 +11,7 @@ fi
 
 APK_DIR="$1/build/app/outputs/flutter-apk"
 
-apksigner sign  \
+${{ vars.BUILD_TOOLS }}/apksigner sign  \
   -v \
   --in $APK_DIR/app-armeabi-v7a$2-release.apk \
   --ks release-keystore.jks \
