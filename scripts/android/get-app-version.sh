2fd2bdbcc146e2f99fde54dda34c9c640c38f86d
sunnydodti <sunnydodti@gmail.com>
2025-01-15 17:41:55 +0530

    fix path to pubspec

--- scripts/android/get-app-version.sh
+++ scripts/android/get-app-version.sh
@@ -5,6 +5,6 @@ if [ -z "$1" ]; then
 echo "Error: Path to pubspec.yml is required." >&2
   exit 1
 fi
-
-APP_VERSION="$(yq -r '.version' $1)-dev"
+PUBSPEC_PATH = $1/pubspec.yaml
+APP_VERSION="$(yq -r '.version' PUBSPEC_PATH)-dev"
 echo "app_version=$APP_VERSION" >> $GITHUB_OUTPUT
