88e61ab272a5c082a073eeb3fda35d791197d3c4
sunnydodti <sunnydodti@gmail.com>
2025-01-15 15:37:06 +0530

    app version script

---
+++ scripts/get-app-version.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+if [ -z "$1" ]; then
+echo "Error: Path to pubspec.yml is required." >&2
+  exit 1
+fi
+
+APP_VERSION="$(yq -r '.version' '$1')-dev"
+echo "app_version=$APP_VERSION" >> $GITHUB_OUTPUT
