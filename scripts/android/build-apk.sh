f90796f6550b9b1142942e548234dcb1c7186bfd
sunnydodti <sunnydodti@gmail.com>
2025-01-16 07:12:47 +0530

    update build apk

--- scripts/android/build-apk.sh
+++ scripts/android/build-apk.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
-#1 = path to repo
-#2 = flavour
-#3 = main file name
+# Arguments:
+# $1 = path to the repo
+# $2 = flavor
+# $3 = main file name
 
+# Check if the first argument (path to repo) is provided
 if [ -z "$1" ]; then
-  echo "reading in base dir ." 
-  $1 = "."
+  echo "Error: Path to the Flutter project is required." >&2
+  exit 1
 fi
-pwd
-cd $1
-echo "running pub get"
-pwd
+
+# Navigate to the Flutter project directory
+echo "Navigating to project directory: $1"
+cd "$1" || { echo "Error: Directory $1 not found."; exit 1; }
+
+# Verify that pubspec.yaml exists
+if [ ! -f "pubspec.yaml" ]; then
+  echo "Error: No pubspec.yaml file found in the directory $1." >&2
+  exit 1
+fi
+
+# Run `flutter pub get`
+echo "Running flutter pub get..."
 flutter pub get
 
-echo "building apk"
-echo "flavour: $2"
-echo "main: $3"
+# Build APK
+echo "Building APK..."
+echo "Flavor: $2"
+echo "Main file: $3"
 
-flutter build apk --flavor $2 -t lib/$3 --split-per-abi
+flutter build apk --flavor "$2" -t "lib/$3" --split-per-abi
