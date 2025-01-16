ee3d95342c27a63a3c2ab349960f1d35b33dc37e
sunnydodti <sunnydodti@gmail.com>
2025-01-16 08:13:18 +0530

    update sign apk

--- scripts/android/sign-apk.sh
+++ scripts/android/sign-apk.sh
@@ -9,12 +9,41 @@ if [ -z "$1" ]; then
   $1 = "."
 fi
 
-APK_DIR="$1/build/app/outputs/flutter-apk"
+KEYSTORE_PASSWORD=$3
+KEY_ALIAS=$4
+KEY_PASSWORD=$5
 
-$BUILD_TOOLS/apksigner sign  \
+if [ -z "$KEY_ALIAS" ] || [ -z "$KEYSTORE_PASSWORD" ] || [ -z "$KEY_PASSWORD" ]; then
+  echo "Error: Missing required parameters or environment variables." >&2
+  exit 1
+fi
+
+APK_DIR="$1/build/app/outputs/flutter-apk"
+APK_SIGNER="$BUILD_TOOLS/apksigner"
+
+echo "signing $APK_DIR/app-armeabi-v7a$2-release.apk"
+$APK_SIGNER sign  \
   -v \
   --in $APK_DIR/app-armeabi-v7a$2-release.apk \
   --ks release-keystore.jks \
   --ks-key-alias $KEY_ALIAS \
-  --ks-pass env:KEYSTORE_PASSWORD \
-  --key-pass env:KEY_PASSWORD
+  --ks-pass "pass:$KEYSTORE_PASSWORD" \
+  --key-pass "pass:$KEY_PASSWORD"
+
+echo "signing $APK_DIR/app-arm64-v8a$2-release.apk"
+$APK_SIGNER sign  \
+  -v \
+  --in $APK_DIR/app-arm64-v8a$2-release.apk \
+  --ks release-keystore.jks \
+  --ks-key-alias $KEY_ALIAS \
+  --ks-pass "pass:$KEYSTORE_PASSWORD" \
+  --key-pass "pass:$KEY_PASSWORD"
+
+echo "signing $APK_DIR/app-x86_64$2-release.apk"
+$APK_SIGNER sign  \
+  -v \
+  --in $APK_DIR/app-x86_64$2-release.apk \
+  --ks release-keystore.jks \
+  --ks-key-alias $KEY_ALIAS \
+  --ks-pass "pass:$KEYSTORE_PASSWORD" \
+  --key-pass "pass:$KEY_PASSWORD"
