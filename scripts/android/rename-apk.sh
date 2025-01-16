a20fe4ef8d7c74760aeda03b302ae9c0de169107
sunnydodti <sunnydodti@gmail.com>
2025-01-16 09:02:48 +0530

    rename - accept appname

--- scripts/android/rename-apk.sh
+++ scripts/android/rename-apk.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # $1 = apk dir
-# $2 = suffix - "-dev"
-# $3 = version
+# $2 = appname
+# $3 = suffix - "-dev"
+# $4 = version
 set -e
 
 if [ -z "$1" ]; then
@@ -9,9 +10,14 @@ if [ -z "$1" ]; then
   $1 = "."
 fi
 
-APK_DIR="$1/build/app/outputs/flutter-apk"
-APP_NAME="${APP_NAME:-App}"
+if [ -z "$2" ]; then
+  echo "reading in base dir ." 
+  $2 = "App"
+fi
 
-mv "$APK_DIR/app-arm64-v8a$2-release.apk" "$APK_DIR/${APP_NAME}_${3}_arm64-v8a-release.apk"
-mv "$APK_DIR/app-armeabi-v7a$2-release.apk" "$APK_DIR/${APP_NAME}_${3}_armeabi-v7a-release.apk"
-mv "$APK_DIR/app-x86_64$2-release.apk" "$APK_DIR/${APP_NAME}_${3}_x86_64-release.apk"
+APK_DIR="$1/build/app/outputs/flutter-apk"
+APP_NAME=$2
+
+mv "$APK_DIR/app-arm64-v8a$3-release.apk" "$APK_DIR/${APP_NAME}_${4}_arm64-v8a-release.apk"
+mv "$APK_DIR/app-armeabi-v7a$3-release.apk" "$APK_DIR/${APP_NAME}_${4}_armeabi-v7a-release.apk"
+mv "$APK_DIR/app-x86_64$3-release.apk" "$APK_DIR/${APP_NAME}_${4}_x86_64-release.apk"
