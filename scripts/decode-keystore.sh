bf69e7605d7c8071bd3cb7702c08dce2cdb795dc
sunnydodti <sunnydodti@gmail.com>
2025-01-16 08:44:36 +0530

    decode keystore - accept base64 inut

--- scripts/decode-keystore.sh
+++ scripts/decode-keystore.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+# $1 - base64 encoded keystore
 
 set -e
+KEYSTORE=$1
+if [ -z "$KEYSTORE" ]; then
+  echo "Encoded keystore is required" >&2
+  exit 1
+fi
 
 # Decode Keystore
 echo "Decoding Keystore..."
